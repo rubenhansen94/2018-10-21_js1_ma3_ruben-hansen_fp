6ccf9c5cc2baebba6d10bacfd9e31847afe58898
Ruben Hansen <rubenhansen94@gmail.com>
2018-10-21 23:13:12 +0200

    Completed LVL1
    Completed level 1 questions, form and printing the elements of the json object. Will look further into how to loop through a json object.

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,63 @@
-//Use RegEx to validate form
+// Use RegEx to validate form
+
+function checkValidation() {
+    var firstName = document.querySelector('#first-name');
+    var lastName = document.querySelector('#last-name');
+    var phone = document.querySelector('#phone');
+    var email = document.querySelector('#email');
+    var phoneExpression = /[0-9]{8,8}$/;
+    var emailExpression = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    var stringExpression = /[a-zA-Z]/;
+
+    // Validation of First Name
+    if (stringExpression.test(firstName.value)) {
+        console.log('First Name is OK!')
+        if (firstName.className === 'invalid') {
+            firstName.classList.toggle('invalid');
+        }
+    } else if (!stringExpression.test(firstName.value)) {
+        console.log('First Name is NOT OK!');
+        if (firstName.className !== 'invalid') {
+            firstName.classList.toggle('invalid');
+        }
+    }
+
+    // Validation of Last Name
+    if (stringExpression.test(lastName.value)) {
+        console.log('Last Name is OK!')
+        if (lastName.className === 'invalid') {
+            lastName.classList.toggle('invalid');
+        }
+    } else if (!stringExpression.test(lastName.value)) {
+        console.log('Last Name is NOT OK!');
+        if (lastName.className !== 'invalid') {
+            lastName.classList.toggle('invalid');
+        }
+    }
+
+    // Validation of Phone Number
+    if (phoneExpression.test(phone.value)) {
+        console.log('Phone Number is OK!')
+        if (phone.className === 'invalid') {
+            phone.classList.toggle('invalid');
+        }
+    } else if (!phoneExpression.test(phone.value)) {
+        console.log('Phone Number is NOT OK!');
+        if (phone.className !== 'invalid') {
+            phone.classList.toggle('invalid');
+        }
+    }
+
+    // Validation of E-mail
+    if (emailExpression.test(email.value)) {
+        console.log('E-mail Name is OK!')
+        if (email.className === 'invalid') {
+            email.classList.toggle('invalid');
+        }
+    } else if (!emailExpression.test(email.value)) {
+        console.log('E-mail is NOT OK!');
+        if (email.className !== 'invalid') {
+            email.classList.toggle('invalid');
+        }
+    }
+}
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,22 @@
 //Console.log out elements in JSON file
+// NB: I use live server with node.js in VSCode and are able to console log it without problem in chrome.
+// Get the json file:
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var jsonFile = JSON.parse(xhttp.responseText);
+        // console.log(jsonFile);
+        console.log(jsonFile.video[0].id);
+        console.log(jsonFile.video[0].name);
+        console.log(jsonFile.video[0].url);
+        // console.log(jsonFile.video[0].author);
+        console.log(jsonFile.video[0].author.data[0].name_author);
+        console.log(jsonFile.video[0].author.data[0].url);
+        console.log(jsonFile.video[0].author.data[0].type);
+        //var entries = object.entries(jsonFile);
+        //console.log(entries);
+    }
+}
+
+xhttp.open("GET", "author.json", true);
+xhttp.send();
